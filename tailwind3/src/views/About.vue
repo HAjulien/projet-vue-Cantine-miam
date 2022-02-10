5efb134ce2f7edd687b511342827ffce5c538d85
HAjulien <hajulien1@gmail.com>
2022-02-10 13:11:15 +0100

    top

--- tailwind3/src/views/About.vue
+++ tailwind3/src/views/About.vue
@@ -2,8 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
 
+  <h1 class="mb-4 text-3xl font-bold text-blue-600 underline">
+    Tailwind CSS 3 with vue js 3
+  </h1>
+  <button class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded ">Next</button>
+  <p class=" font-extrabold text-center underline decoration-sky-500 ">The quick brown fox...</p>
 
-    
+
   </div>
 
 
