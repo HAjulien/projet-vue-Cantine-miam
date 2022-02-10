aeda8ca74f6bfecb301d031b7d5d9af09557135d
HAjulien <hajulien1@gmail.com>
2022-02-10 12:12:06 +0100

    ok

--- vue3-tailwindcss/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './styles/main.css' // Make sure this line is added so Tailwind will be globally available in your Vue components
-
-
-createApp(App).use(router).mount('#app')
