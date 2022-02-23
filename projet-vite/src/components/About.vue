71de4c969206597ef00b84b5de955cc9baabdf6f
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-23 15:22:37 +0100

    ok

--- projet-vite/src/components/About.vue
+++ projet-vite/src/components/About.vue
@@ -1,8 +1,8 @@
 /<template>
 <div>
 
-    <h1 class="font-bold text-2xl">
-        About {{ name }}
+    <h1 class="font-bold text-2xl ">
+        About {{ name }} 
     </h1>
 </div>
   
--- projet-vite/src/components/Home.vue
+++ projet-vite/src/components/Home.vue
@@ -1,15 +1,17 @@
-<template>
-  <div>
-    <h1 class="font-extrabold text-3xl mb-8">home</h1>
-    <p class="mb-6">  Name in store is: {{ name }} </p>
-
-    <input  v-model="newName"
-     type="text" 
-     class="p-2 mr-3 border border-gray-600 rounded"
-     />
-
-    <button @click="saveName" class="p-2 text-white bg-indigo-600 rounded">Envoyé</button>
-
+<template >
+  <div class="dark">
+    <div class="dark:bg-gray-600 bg-pink-300 ">
+      <h1 class=" dark:text-white  font-extrabold text-3xl mb-8 m-3 ">home</h1>
+      <p class="mb-6 dark:text-white m-3 ">  Name in store is: {{ name }} </p>
+      
+      <input  v-model="newName"
+       type="text" 
+       class="p-2 m-3 border border-gray-600 rounded"
+       />
+      
+      <button @click="saveName" class=" dark:bg-pink-600 p-2 text-white bg-fuchsia-500 rounded">Envoyé</button>
+      
+    </div>
   </div>
 </template>
 
