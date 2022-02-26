afb46164d9c8d05f68d2f759e3f7b3f53c7fb386
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-26 11:54:44 +0100

    Update Home.vue

--- projet-vite/src/views/Home.vue
+++ projet-vite/src/views/Home.vue
@@ -1,12 +1,21 @@
 /<template>
   <div>
         <ProgressBar :progress='progress'/>
+        <div >
+            <div class="text-center">
+                <transition name="bounce">
+                    <p v-show="isActive" class="tt">
+                            test
+                    </p>
+                </transition>
+            </div>
             <button 
             class=" zaa text-white font-bold py-2 px-4 rounded"
             @click="isActive = !isActive"
             :class="[isActive ? 'bg-lime-300' : 'bg-pink-300']"
             >{{ isActive ? 'menu' : 'x' }}
             </button>
+        </div>
 
         <Post @progressUpdate="setProgress"/>
   </div>
@@ -50,6 +59,24 @@ export default {
         transition: all .4s linear;
     }
 
+    .bounce-enter-active {
+    animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+    }
+    @keyframes bounce-in {
+    0% {
+        transform: scale(0);
+    }
+    50% {
+        transform: scale(1.5);
+    }
+    100% {
+        transform: scale(1);
+    }
+
+    }
 
 
 </style>
