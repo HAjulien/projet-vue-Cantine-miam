22cac26c4103edcc0cd97c218322cb25660d6ecc
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-02 13:49:28 +0100

    ok

--- projet-vite/src/components/Articles.vue
+++ projet-vite/src/components/Articles.vue
@@ -5,10 +5,10 @@
         <Feature v-for="(feature, index) in features" :key="index" :title="feature.title" :image="feature.image" :paragraphe="feature.paragraphe" :button="feature.button"/>
     </div>
 
-  <div class="sm:flex items-center  shadow-md m-2 border-2 p-4 bg-gray-100">
+  <div class="sm:flex items-center  shadow-md m-2 border-2 p-4 bg-gray-100 dark:bg-gray-500 ">
     <div class="md:px-10 sm:px-5 ">
-      <h1 class="text-gray-800 font-bold text-2xl my-2 dark:text-black">long established</h1>
-      <p class="text-gray-700 mb-2 md:mb-6 dark:text-gray-300">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
+      <h1 class="text-center font-bold text-2xl my-2 dark:text-black">long established</h1>
+      <p class="text-gray-700 mb-2 md:mb-6 dark:text-slate-100">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
      
     </div>
     <div class="">
--- projet-vite/src/components/Feature.vue
+++ projet-vite/src/components/Feature.vue
@@ -6,11 +6,11 @@
   <div class=" card md:width-[45vw] cards flex md:inline-flex space-x-6 m-2 mb-7 dark:bg-gray-500  inline-block	border-solid border-2 bg-gray-100	">
     <div class="shadow-md">
       <div class="p-4">
-        <h1 class="mt-3 text-gray-800 text-2xl font-bold my-2 dark:text-black">{{title}}</h1>
-        <p class="text-gray-700 mb-2  dark:text-slate-300">{{ paragraphe }}</p>
+        <h1 class="mt-3 text-2xl text-center font-bold my-2 dark:text-black">{{title}}</h1>
+        <p class="text-gray-800 mb-2 indent-5  dark:text-slate-100">{{ paragraphe }}</p>
         
         <div class="flex justify-between items-center mt-4 pb-2">
-          <span class="mb-2 text-gray-800 font-bold dark:text-slate-300">Découvrir: </span>
+          <span class="mb-2 text-gray-800 font-bold dark:text-slate-100">Découvrir: </span>
           <button class="p-1 pl-2 pr-2 bg-transparent border-2 border-green-500 text-green-500  rounded-lg hover:bg-green-500 hover:text-gray-100 focus:border-4 focus:border-green-300">{{ button }} </button>
         </div>
         <img v-bind:src="'src/assets/images/' + image" />
