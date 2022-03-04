920822af9656a92699e1f0aed852ddfc00c87f6d
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-04 23:24:33 +0100

    ok

--- projet-vite/src/components/Feature.vue
+++ projet-vite/src/components/Feature.vue
@@ -3,13 +3,13 @@
 
 <!-- component -->
   
-  <div class=" card md:width-[45vw] cards flex md:inline-flex space-x-6 m-2 mb-12 dark:bg-gray-500  inline-block	border-solid border-2 bg-gray-200	rounded-xl">
+  <div class=" card md:width-[45vw] cards flex md:inline-flex space-x-6 m-2 mb-12 dark:bg-gray-500  inline-block	border-solid border-2 bg-gray-200	rounded-xl  hover:bg-gray-100 hover:border-4 hover:border-amber-300 	">
     <div class="shadow-md rounded-xl ">
       <div class="p-4 text-lg">
         <h1 class="mt-3 text-2xl flex items-center justify-center text-center md:h-28 font-extrabold my-2 dark:text-black">{{title}}</h1>
         <p class="text-gray-800 mb-6 mt-8  indent-5 dark:text-slate-200">{{ paragraphe }}</p>
         
-        <div class="flex justify-between items-center mt-4 pb-2">
+        <div class="flex justify-between items-center mt-4 mb-7 pb-2">
           <span class="mb-2 text-gray-800 font-bold dark:text-slate-200">Découvrir: </span>
            <router-link :to="{path: chemin }" >
             <button class=" mb-3 p-1 pl-2 pr-2 bg-transparent border-2 border-green-600 text-green-500  rounded-lg hover:bg-green-600 hover:text-gray-200 focus:border-4 focus:border-green-300">
@@ -17,7 +17,7 @@
             </button>
           </router-link>  
         </div> 
-          <img v-bind:src="'src/assets/images/' + image"/>
+          <img v-bind:src="'src/assets/images/' + image" class="mb-3"/>
         </div>
       </div>
     </div>
@@ -41,6 +41,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.card{
+  transition: all .2s linear;
+}
+.card:hover{
+  transition: all .2s linear;  
+}
+
+
 @media screen and (min-width:768px){
 
   .card{
@@ -50,9 +59,16 @@ export default {
 
 }
 
-@media screen and (min-width:766px) and (max-width:1060px) {
+@media screen and (min-width:766px) and (max-width:1300px) {
   p{
     height: 250px;
+  }
+
+}
+
+@media screen and (min-width:1301px) {
+  p{
+    height: 200px;
   }
 
 }
