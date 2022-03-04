cf9f6ffbeff5faa1c8f5c275c36ede41c918b257
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-04 15:25:52 +0100

    ok

--- projet-vite/src/components/HeaderMobile.vue
+++ projet-vite/src/components/HeaderMobile.vue
@@ -1,22 +1,27 @@
 <template>
-    <header class=" md:hidden lg:hidden headerMobile flex justify-between items-center px-2 dark:bg-gray-700 bg-gray-200	 h-16">
-        <router-link to="/" ><img src="../assets/images/logo.png" class="w-24 h-10" /> </router-link> 
-        <div class=" flex justify-around items-center w-32">
-            <BtnDarkMode/>
-            <router-link to="" class=" flex items-center ml-3 ">
-                            <fa :style="{height: '24px', color: '#097770', }"   icon='user' />
-            </router-link>
-        </div>
-    </header>
+<div>
+        <header class=" md:hidden lg:hidden headerMobile flex justify-between items-center px-2 dark:bg-gray-700 bg-gray-200	 h-16">
+            <router-link to="/" ><img src="../assets/images/logo.png" class="w-24 h-10" /> </router-link> 
+            <div class=" flex justify-around items-center w-32">
+                <BtnDarkMode/>
+                <router-link to="" class=" flex items-center ml-3 ">
+                                <fa :style="{height: '24px', color: '#097770', }"   icon='user' />
+                </router-link>
+            </div>
+        </header>
+        <ProfileModale/>
+</div>
 </template>
 
 <script>
-import BtnDarkMode from './BntDarkMode.vue'
+import BtnDarkMode from './BntDarkMode.vue';
+import ProfileModale from './ProfileModale.vue';
 
 export default {
     name:'HeaderMobile',
     components: {
         BtnDarkMode,
+        ProfileModale,
     },
 
 }
---
+++ projet-vite/src/components/ProfileModale.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bloc-modale">
+
+        <div class="overlay"></div>
+
+        <div class="modale card">
+            <div class="bnt-modale"></div>
+            <h2>contenu de la modale</h2>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ProfileModale',
+
+
+}
+</script>
+
+<style>
+
+</style>
