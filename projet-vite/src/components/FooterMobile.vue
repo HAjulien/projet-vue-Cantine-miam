f8c99ff2db2fff017f00a689ed6e20d55c118444
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-19 18:43:25 +0100

    Revert "json pour footer mobile"
    This reverts commit 691f569b229162a951fbfc89a0181641ecf79b2b.

--- projet-vite/src/components/FooterMobile.vue
+++ projet-vite/src/components/FooterMobile.vue
@@ -4,13 +4,31 @@
         <p class="text-center text-xl m-3" > nous suivre sur :</p>
 
         <ul class=" flex justify-around">
-            <li
-            v-for="icone in icones" v-bind:key="icone.id ">              
 
-                <a :href="icone.href" target="_blank " @click=" icone.number = !icone.number">
-                    <fa :style="{ height: '40px'}" :icon="[ 'fab', icone.fa ]" :class="[icone.number ? icone.color1 : icone.color2 ]"/>
+            <li>
+                <a href="https://www.facebook.com" target="_blank " @click=" icon1 = !icon1">
+                    <fa :style="{ height: '40px'}" :icon="[ 'fab', 'facebook' ]" :class="[icon1 ? 'text-blue-800' : 'text-blue-300']"/>
                 </a>       
-            </li> 
+            </li>
+
+            <li>
+                <a href="https://www.instagram.com" target="_blank" @click=" icon2 = !icon2">
+                    <fa :style="{ height: '40px'}" :icon="[ 'fab', 'instagram' ]" :class="[icon2 ? 'text-violet-700' : 'text-stone-400']"/>
+                </a>
+            </li>
+
+            <li>
+                <a href="https://twitter.com" target="_blank" @click=" icon3 = !icon3">
+                    <fa :style="{ height: '40px'}" :icon="[ 'fab', 'twitter' ]" :class="[icon3 ? 'text-sky-400' : 'text-zinc-400']"/>
+                </a> 
+            </li>
+
+            <li>
+                <a href="https://youtube.com" target="_blank" @click=" icon4 = !icon4">
+                    <fa :style="{ height: '40px'}" :icon="[ 'fab', 'youtube' ]" :class="[icon4 ? 'text-red-600' : 'text-amber-600']"/>
+                </a> 
+            </li>
+
         </ul>
 
         <p class="text-center mt-4">CANTINE MIAM tous droits réservés, 2022</p>
@@ -19,7 +37,7 @@
 </template>
 
 <script>
-import axios from "axios";
+
 
 export default {
     name: 'FooterMobile',
@@ -31,13 +49,7 @@ export default {
         icon4: true,
         progress:0,
 
-        icones: [],
         }
-    },
-    created () {
-        axios
-        .get("http://localhost:3000/icones.json")
-        .then (response => (this.icones = response.data.icones))
     },
 }
 
