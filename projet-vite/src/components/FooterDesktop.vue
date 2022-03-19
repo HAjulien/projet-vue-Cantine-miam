4d4851a966045de81665cf93c9bc5a024af66f97
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-19 18:44:51 +0100

    Update FooterDesktop.vue

--- projet-vite/src/components/FooterDesktop.vue
+++ projet-vite/src/components/FooterDesktop.vue
@@ -3,10 +3,22 @@
 
     <ul class="flex justify-around items-center icone mx-0.5 mb-2">
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
         <li
         v-for="icone in icones" v-bind:key="icone.id ">              
             <a :href="icone.href" target="_blank " @click=" icone.number = !icone.number">
                 <fa :style="{ height: '40px'}" :icon="[ 'fab', icone.fa ]" :class="[icone.number ? icone.color1 : icone.color2 ]"/>
+=======
+        <li>
+            <a href="https://www.facebook.com" target="_blank " @click=" icon1 = !icon1">
+                <fa :style="{ height: '40px'}" :icon="[ 'fab', 'facebook' ]" :class="[icon1 ? 'text-blue-800' : 'text-blue-300']"/>
+>>>>>>> parent of 02892092 (rendu footerDesktop dynamique)
+=======
+        <li>
+            <a href="https://www.facebook.com" target="_blank " @click=" icon1 = !icon1">
+                <fa :style="{ height: '40px'}" :icon="[ 'fab', 'facebook' ]" :class="[icon1 ? 'text-blue-800' : 'text-blue-300']"/>
+>>>>>>> parent of 02892092 (rendu footerDesktop dynamique)
 =======
         <li>
             <a href="https://www.facebook.com" target="_blank " @click=" icon1 = !icon1">
@@ -93,12 +105,18 @@ export default {
         icon3: true,
         icon4: true,
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
         icones:[
             {href: 'https://www.facebook.com', fa: 'facebook', number:'icon1' , color1:'text-blue-800', color2:'text-blue-300', id:1},
             {href: 'https://www.instagram.com', fa: 'instagram', number:'icon2' , color1:'text-violet-700', color2:'text-stone-400', id:2},
             {href: 'https://www.twitter.com', fa: 'twitter', number:'icon3' , color1:'text-sky-400', color2:'text-zinc-400', id:3},
             {href: 'https://www.youtube.com', fa: 'youtube', number:'icon4' , color1:'text-red-600', color2:'text-amber-600', id:4},
         ],
+=======
+>>>>>>> parent of 02892092 (rendu footerDesktop dynamique)
+=======
+>>>>>>> parent of 02892092 (rendu footerDesktop dynamique)
 =======
 >>>>>>> parent of 02892092 (rendu footerDesktop dynamique)
         infos: [
