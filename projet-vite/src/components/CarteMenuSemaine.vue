a4b77463baccba29c4c3bd3b566c5cc1fdda4bd8
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-07-17 23:18:20 +0200

    test

--- projet-vite/src/components/CarteMenuSemaine.vue
+++ projet-vite/src/components/CarteMenuSemaine.vue
@@ -2,14 +2,14 @@
     <div>
         <router-link :to="'/platDetail/' + id">
             <article class="cursor-auto	">
-                <div v-if="categorie == type"  class=" p-2 flex flex-col items-center justify-between  m-2 md:h-[300px]  md:w-[200px] lg:w-[240px] md:mx-6 rounded-lg transition-all hover:transition-all hover:bg-amber-400/30"
+                <div v-if="categorie == type"  class=" p-2 flex flex-col items-center justify-between  m-2 space-y-4 md:h-[300px]  md:w-[200px] lg:w-[240px] md:mx-6 rounded-lg transition-all hover:transition-all hover:bg-amber-400/30  dark:bg-gray-600"
                 :style="{border:`3px solid ${couleur}` }"
                 >
-                    <img :src="image" :alt="altImage" class="w-48 rounded-lg md:h-40 " >
+                    <img :src="image" :alt="altImage" class="w-[80%] rounded-lg md:w-48 md:h-40 " >
                     <h3 class="text-xl text-center  underline underline-offset-2 decoration-[3px] "
                     :style="{textDecorationColor:couleur }"
                     > {{nom}} </h3>
-                    <span class="px-3 py-0.5 bg-emerald-200 rounded-xl"> {{prix}} €</span>
+                    <span class="px-3 py-0.5 bg-emerald-200 rounded-xl dark:bg-emerald-700"> {{prix}} €</span>
                 </div>
             </article>
         </router-link>
--- projet-vite/src/views/MenuSemaine.vue
+++ projet-vite/src/views/MenuSemaine.vue
@@ -1,13 +1,25 @@
 <template>
-    <div id="app">
+    <div id="app" class="dark:text-gray-100 dark:bg-gray-700">
 
-        <h2 class="text-center text-3xl border-2 border-[#A40E4C] w-72 ml-[50vw] translate-x-[-50%] my-6">LES ENTRÉES</h2>
+        <ul class="flex justify-center items-center space-x-4 my-2 flex-wrap	">
+            <li
+            v-for="selection in selections" :key="selection.id ">              
+                <button class="bg-red-500 px-3 py-0.5 rounded-lg flex my-2 "
+                :class="selection.id == jourJS ? 'bg-gray-200' : '' "
+                @click="changeJour()"
+                >
+                    {{selection.nom}} 
+                </button>       
+            </li>
+        </ul>
+
+        <h2 class="text-center text-3xl border-2 border-[#A40E4C] w-72 ml-[50vw] translate-x-[-50%] my-6 py-2 dark:bg-gray-600">LES ENTRÉES</h2>
         <div class="md:flex md:items-center md:justify-center  m-auto">
             <CarteMenuSemaine v-for="(produit, index) in produits" :key="index" :categorie="produit.category.nom" :couleur="produit.category.couleur"  :type="'entrée'"   :image="produit.image" :altImage="produit.altImage" :nom="produit.nom" :prix="produit.prixAchat" :id="produit.id"
             />
         </div>
 
-        <h2 class="text-center text-3xl border-2 border-[#A40E4C] w-72 ml-[50vw] translate-x-[-50%] my-6">LES PLATS</h2>
+        <h2 class="text-center text-3xl border-2 border-[#A40E4C] w-72 ml-[50vw] translate-x-[-50%] my-6 py-2 dark:bg-gray-600">LES PLATS</h2>
         <div class="md:flex md:items-center md:justify-center  m-auto">
             <CarteMenuSemaine v-for="(produit, index) in produits" :key="index" :categorie="produit.category.nom" :couleur="produit.category.couleur"  :type="'plat poisson'"   :image="produit.image" :altImage="produit.altImage" :nom="produit.nom" :prix="produit.prixAchat" :id="produit.id"
             />         
@@ -17,7 +29,7 @@
             />
         </div>
         
-        <h2 class="text-center text-3xl border-2 border-[#A40E4C] w-72 ml-[50vw] translate-x-[-50%] my-6">LES DESSERT</h2>
+        <h2 class="text-center text-3xl border-2 border-[#A40E4C] w-72 ml-[50vw] translate-x-[-50%] my-6 py-2 dark:bg-gray-600">LES DESSERT</h2>
         <div class="md:flex md:items-center md:justify-center  m-auto">
             <CarteMenuSemaine v-for="(produit, index) in produits" :key="index" :categorie="produit.category.nom" :couleur="produit.category.couleur"  :type="'dessert'"   :image="produit.image" :altImage="produit.altImage" :nom="produit.nom" :prix="produit.prixAchat" :id="produit.id"
             />
@@ -40,6 +52,14 @@ export default {
             lienAPI: "https://cantinemiam.herokuapp.com",
             jourJS: '',
             produits: [],
+            selections:[
+                { id:1, nom:'Lundi' },
+                { id:2, nom:'Mardi' },
+                { id:3, nom:'Mercredi' },
+                { id:4, nom:'Jeudi' },
+                { id:5, nom:'Vendredi' },
+                { id:8, nom:'Boissons' },
+            ],
         }
     },
 
@@ -57,6 +77,13 @@ export default {
         .then(console.log(this.produits))
         .then(console.log(`${this.lienAPI}/api/produits?page=1&JourPrevu=${this.JourJS}&category.id=2`))
     },
+
+    methods: {
+        changeJour(){
+            console.log(selection.id);
+        }
+    },
+
 
 }
 </script>
