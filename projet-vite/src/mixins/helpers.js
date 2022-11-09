018d094bbe176e405306f25a577cfebae04ef27f
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-11-09 19:30:10 +0100

    Update helpers.js

--- projet-vite/src/mixins/helpers.js
+++ projet-vite/src/mixins/helpers.js
@@ -1,7 +1,7 @@
 export const calculateMoyenne = (critiquesObject) => {
     const sommeAllNotes = critiquesObject
-        .map( critique => critique.note )   //note est obligatoire
-        .reduce((sommeNotes, note) => sommeNotes += note ) 
-        const moyenne = sommeAllNotes / critiquesObject.length
-        return moyenne
-}
+        .map((critique) => critique.note) //note est obligatoire
+        .reduce((sommeNotes, note) => (sommeNotes += note));
+    const moyenne = sommeAllNotes / critiquesObject.length;
+    return moyenne;
+};
